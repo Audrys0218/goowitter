8041497effb10dafa1d930086420b09cf322fe6b
Audrius Danielievicius <Audrys0218@gmail.com>
2014-03-18 16:13:46 +0200

    Project structure.

---
+++ goowitter/goowitter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="goowitter.MvcApplication" Language="C#" %>
---
+++ goowitter/goowitter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(goowitter.Startup))]
+namespace goowitter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
